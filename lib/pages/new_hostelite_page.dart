4d07c8deb062f3f5447ddab23f84fc53cc81dd53
Manish <111450082+ManishModak@users.noreply.github.com>
2023-05-23 12:35:34 +0530

    Update new_hostelite_page.dart
    Makeover of New Hostelite page UI.

--- lib/pages/new_hostelite_page.dart
+++ lib/pages/new_hostelite_page.dart
@@ -45,66 +45,120 @@ void _submitForm() async {
 @override
 Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black45,
       appBar: AppBar(
-        backgroundColor: Colors.black87,
         title: const Text("New Hostelite"),
         centerTitle: true,
         elevation: 0,
       ),
-      body: Container(
-        padding:const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-        color: Colors.grey[800],
-        child: Column(
-          children: [
-            Center(
-              child: CupertinoButton(
-                onPressed: () async {
-                  XFile? selectedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+      body: SingleChildScrollView(
+        child: Container(
+          padding:const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+          child: Column(
+            children: [
+              Center(
+                child: CupertinoButton(
+                  onPressed: () async {
+                    XFile? selectedImage = await ImagePicker().pickImage(source: ImageSource.camera);
 
-                  if(selectedImage != null){
-                    File convertedImage = File(selectedImage.path);
-                    setState(() {
-                      studentPic = convertedImage;
-                    });
-                  }
-                  else{
-                    if (kDebugMode) {
-                      print("Image not selected");
+                    if(selectedImage != null){
+                      File convertedImage = File(selectedImage.path);
+                      setState(() {
+                        studentPic = convertedImage;
+                      });
                     }
-                  }
+                    else{
+                      if (kDebugMode) {
+                        print("Image not selected");
+                      }
+                    }
+                  },
+                  child: CircleAvatar(
+                    backgroundImage: (studentPic != null) ? FileImage(studentPic!) : null,
+                    radius: 80,
+                    backgroundColor: Colors.grey,
+                  ),
+                ),
+              ),
+              const Divider(
+                height: 40.0,
+              ),
+              TextFormField(
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+                decoration: textInputDecoration.copyWith(hintText: "ID"),
+                validator: (val) => val == null || val.isEmpty ? "Enter ID" : null,
+                onChanged: (val) {
+                  setState(() => id = val.toUpperCase().trim());
                 },
-                child: CircleAvatar(
-                  backgroundImage: (studentPic != null) ? FileImage(studentPic!) : null,
-                  radius: 80,
-                  backgroundColor: Colors.grey,
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+                decoration: textInputDecoration.copyWith(hintText: "Name"),
+                validator: (val) => val == null || val.isEmpty ? "Enter Name" : null,
+                onChanged: (val) {
+                  String capName = val[0].toUpperCase() + val.substring(1) ;
+                  setState(() => name = capName.trim());
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+                decoration: textInputDecoration.copyWith(hintText: "Room No"),
+                validator: (val) => val == null || val.isEmpty ? "Enter Room No" : null,
+                onChanged: (val) {
+                  setState(() => roomNo = val.toUpperCase().trim());
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+                decoration: textInputDecoration.copyWith(hintText: "Branch"),
+                validator: (val) => val == null || val.isEmpty ? "Enter Branch" : null,
+                onChanged: (val) {
+                  setState(() => branch = val.toUpperCase().trim());
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                ),
+                decoration: textInputDecoration.copyWith(hintText: "Mobile No"),
+                validator: (val) => val == null || val.isEmpty ? "Enter Mobile No" : null,
+                onChanged: (val) {
+                  setState(() => mobileNo = val.toUpperCase().trim());
+                },
+              ),
+              const SizedBox(height: 40.0),
+              ElevatedButton(
+                onPressed: _submitForm ,
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(const Size(150, 50)),
+                ),
+                child: const Text(
+                  "Submit",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
                 ),
               ),
-            ),
-            const Divider(
-              height: 40.0,
-            ),
-            TextFormField(
-              style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20
-              ),
-              decoration: textInputDecoration.copyWith(hintText: "ID"),
-              validator: (val) => val == null || val.isEmpty ? "Enter ID" : null,
-              onChanged: (val) {
-                setState(() => id = val.toUpperCase().trim());
-              },
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _submitForm ,
-              child: const Text(
-                "Submit",
-                style: TextStyle(
-                  color: Colors.white
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       )
     );
