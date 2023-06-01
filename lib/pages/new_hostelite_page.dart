1264df0a9002b839af80ac07c8153a50395d6f92
Manish <manishmodak90@gmail.com>
2023-06-02 00:17:08 +0530

    New Improved UI ,validation check for new hostelite and new manual Entry page

--- lib/pages/new_hostelite_page.dart
+++ lib/pages/new_hostelite_page.dart
@@ -18,24 +18,32 @@ class NewStudent extends StatefulWidget {
 
 class _NewStudentState extends State<NewStudent> {
 
-  String id = '';
-  String name = '';
-  String roomNo = '';
-  String branch = '';
-  String mobileNo = '';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>() ;
+
+  final TextEditingController id = TextEditingController();
+  final TextEditingController name = TextEditingController();
+  final TextEditingController roomNo = TextEditingController();
+  final TextEditingController branch = TextEditingController();
+  final TextEditingController mobileNo = TextEditingController();
 
   File? studentPic;
   double percentage=0;
+
 @override
 void dispose() {
   super.dispose();
+  id.dispose();
+  name.dispose();
+  roomNo.dispose();
+  branch.dispose();
+  mobileNo.dispose();
 }
 
-void _submitForm() async {
+void _submitForm() async{
 
   MainDatabase student = MainDatabase();
 
-  UploadTask uploadTask = FirebaseStorage.instance.ref().child("pictures").child(id).child(id).putFile(studentPic!);
+  UploadTask uploadTask = FirebaseStorage.instance.ref().child("pictures").child(id.text).child(id.text).putFile(studentPic!);
 
   uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
     setState(() {
@@ -45,152 +53,174 @@ void _submitForm() async {
   TaskSnapshot taskSnapshot = await uploadTask;
   String downloadUrl =  await taskSnapshot.ref.getDownloadURL();
 
-  student.addStudent(id: id, name: name, room: roomNo, branch: branch, mobile: mobileNo, url: downloadUrl);
+  student.addStudent(
+      id: id.text ,
+      name: name.text,
+      room: roomNo.text,
+      branch: branch.text,
+      mobile: mobileNo.text,
+      url: downloadUrl
+  );
+
   setState(() {
     percentage = 0;
+    studentPic = Null as File? ;
   });
+
+  id.clear();
+  name.clear();
+  roomNo.clear();
+  branch.clear();
+  mobileNo.clear();
 }
 
 @override
 Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.black45,
-        appBar: AppBar(
-          backgroundColor: Colors.red[400],
-          title: const Text(
-              "New Hostelite",
-            style: TextStyle(letterSpacing: 1.25),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: appBarGradient
+                ),
+              ),
+              buildAppBar("New Hostilite")
+            ],
           ),
-          centerTitle: true,
-          elevation: 0,
         ),
         body: SingleChildScrollView(
           child: Container(
             padding:const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-            child: Column(
-              children: [
-                Center(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox(
-                        width: 180,
-                        height: 180,
-                        child: CircularProgressIndicator(
-                          value: percentage,
-                          backgroundColor: Colors.black45,
-                          strokeWidth: 5,
-                          valueColor: const AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Center(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          width: 180,
+                          height: 180,
+                          child: CircularProgressIndicator(
+                            value: percentage,
+                            backgroundColor: Colors.black45,
+                            strokeWidth: 5,
+                            valueColor: const AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+                          ),
                         ),
-                      ),
-                      CupertinoButton(
-                        onPressed: () async {
-                          percentage = 0;
-                          XFile? selectedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+                        CupertinoButton(
+                          onPressed: () async {
+                            percentage = 0;
+                            XFile? selectedImage = await ImagePicker().pickImage(source: ImageSource.camera);
 
-                          if(selectedImage != null){
-                            File convertedImage = File(selectedImage.path);
-                            setState(() {
-                              studentPic = convertedImage;
-                            });
-                          }
-                          else{
-                            if (kDebugMode) {
-                              print("Image not selected");
+                            if(selectedImage != null){
+                              File convertedImage = File(selectedImage.path);
+                              setState(() {
+                                studentPic = convertedImage;
+                              });
                             }
-                          }
-                        },
-                        child: CircleAvatar(
-                          backgroundImage: (studentPic != null) ? FileImage(studentPic!) : null,
-                          radius: 80,
-                          backgroundColor: Colors.grey,
+                            else{
+                              if (kDebugMode) {
+                                print("Image not selected");
+                              }
+                            }
+                          },
+                          child: CircleAvatar(
+                            backgroundImage: (studentPic != null) ? FileImage(studentPic!) : null,
+                            radius: 80,
+                            backgroundColor: Colors.grey,
+                          ),
                         ),
-                      ),
-                    ]
-                  ),
-                ),
-                const Divider(
-                  height: 40.0,
-                ),
-                TextFormField(
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20
-                  ),
-                  decoration: textInputDecoration.copyWith(hintText: "ID"),
-                  validator: (val) => val == null || val.isEmpty ? "Enter ID" : null,
-                  onChanged: (val) {
-                    setState(() => id = val.toUpperCase().trim());
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20
-                  ),
-                  decoration: textInputDecoration.copyWith(hintText: "Name"),
-                  validator: (val) => val == null || val.isEmpty ? "Enter Name" : null,
-                  onChanged: (val) {
-                    String capName = val[0].toUpperCase() + val.substring(1) ;
-                    setState(() => name = capName.trim());
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20
-                  ),
-                  decoration: textInputDecoration.copyWith(hintText: "Room No"),
-                  validator: (val) => val == null || val.isEmpty ? "Enter Room No" : null,
-                  onChanged: (val) {
-                    setState(() => roomNo = val.toUpperCase().trim());
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20
-                  ),
-                  decoration: textInputDecoration.copyWith(hintText: "Branch"),
-                  validator: (val) => val == null || val.isEmpty ? "Enter Branch" : null,
-                  onChanged: (val) {
-                    setState(() => branch = val.toUpperCase().trim());
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20
-                  ),
-                  decoration: textInputDecoration.copyWith(hintText: "Mobile No"),
-                  validator: (val) => val == null || val.isEmpty ? "Enter Mobile No" : null,
-                  onChanged: (val) {
-                    setState(() => mobileNo = val.toUpperCase().trim());
-                  },
-                ),
-                const SizedBox(height: 40.0),
-                ElevatedButton(
-                  onPressed: _submitForm ,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color?>(Colors.redAccent),
-                    minimumSize: MaterialStateProperty.all<Size>(const Size(130, 50)),
-                  ),
-                  child: const Text(
-                    "Submit",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 1.5
+                      ]
                     ),
                   ),
-                ),
-              ],
+                  const Divider(
+                    height: 40.0,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20
+                    ),
+                    decoration: textInputDecoration.copyWith(hintText: "ID"),
+                    validator: (val) => val == null || val.isEmpty ? "Enter ID" : null,
+                    controller: id,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20
+                    ),
+                    decoration: textInputDecoration.copyWith(hintText: "Name"),
+                    validator: (val) => val == null || val.isEmpty ? "Enter Name" : null,
+                    controller: name,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20
+                    ),
+                    decoration: textInputDecoration.copyWith(hintText: "Room No"),
+                    validator: (val) => val == null || val.isEmpty ? "Enter Room No" : null,
+                    controller: roomNo,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20
+                    ),
+                    decoration: textInputDecoration.copyWith(hintText: "Branch"),
+                    validator: (val) => val == null || val.isEmpty ? "Enter Branch" : null,
+                    controller: branch,
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20
+                    ),
+                    decoration: textInputDecoration.copyWith(hintText: "Mobile No"),
+                    validator: (val) => val == null || val.isEmpty ? "Enter Mobile No" : null,
+                    controller: mobileNo,
+                  ),
+                  const SizedBox(height: 40.0),
+                  Container(
+                    width: 180,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: buttonLinearGradient
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () async{
+                        if(_formKey.currentState!.validate()){
+                          _submitForm();
+                        }
+                      } ,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0
+                      ),
+                      child: const Text(
+                        "Submit",
+                        style: TextStyle(
+                          fontSize: 23,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.5
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         )
